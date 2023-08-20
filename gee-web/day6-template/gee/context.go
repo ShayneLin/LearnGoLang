5fd0b116026e49d6a959b8546f65f39a22871e50
Shayne <740074513@qq.com>
2023-08-20 12:40:22 +0800

    feat:day6-template

--- gee-web/day6-template/gee/context.go
+++ gee-web/day6-template/gee/context.go
@@ -18,6 +18,7 @@ type Context struct {
 	index      int
 	// middleware
 	handlers []HandlerFunc
+	engine   *Engine
 }
 
 func newContext(w http.ResponseWriter, req *http.Request) *Context {
@@ -75,10 +76,12 @@ func (c *Context) JSON(code int, obj interface{}) {
 	}
 }
 
-func (c *Context) HTML(code int, html string) {
+func (c *Context) HTML(code int, name string, data interface{}) {
 	c.SetHeader("Content-Type", "text/html")
 	c.Status(code)
-	c.Writer.Write([]byte(html))
+	if err := c.engine.htmlTemplates.ExecuteTemplate(c.Writer, name, data); err != nil {
+		c.Fail(500, err.Error())
+	}
 }
 
 func (c *Context) Param(key string) string {
