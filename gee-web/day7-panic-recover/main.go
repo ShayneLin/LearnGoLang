7c8f97d0f39356817f36afe82dfdba5f05925252
Shayne <740074513@qq.com>
2023-08-20 16:55:14 +0800

    feat:day07异常处理

--- gee-web/day7-panic-recover/main.go
+++ gee-web/day7-panic-recover/main.go
@@ -1,36 +1,35 @@
 package main
 
 /*
-(1) render array
-$ curl http://localhost:9999/date
-<html>
-<body>
-    <p>hello, gee</p>
-    <p>Date: 2019-08-17</p>
-</body>
-</html>
-*/
+$ curl "http://localhost:9999"
+Hello Geektutu
+$ curl "http://localhost:9999/panic"
+{"message":"Internal Server Error"}
+$ curl "http://localhost:9999"
+Hello Geektutu
 
-/*
-(2) custom render function
-$ curl http://localhost:9999/students
-<html>
-<body>
-    <p>hello, gee</p>
-    <p>0: Geektutu is 20 years old</p>
-    <p>1: Jack is 22 years old</p>
-</body>
-</html>
-*/
+>>> log
+2020/01/09 01:00:10 Route  GET - /
+2020/01/09 01:00:10 Route  GET - /panic
+2020/01/09 01:00:22 [200] / in 25.364µs
+2020/01/09 01:00:32 runtime error: index out of range
+Traceback:
+        /usr/local/Cellar/go/1.12.5/libexec/src/runtime/panic.go:523
+        /usr/local/Cellar/go/1.12.5/libexec/src/runtime/panic.go:44
+        /Users/7days-golang/day7-panic-recover/main.go:47
+        /Users/7days-golang/day7-panic-recover/gee/context.go:41
+        /Users/7days-golang/day7-panic-recover/gee/recovery.go:37
+        /Users/7days-golang/day7-panic-recover/gee/context.go:41
+        /Users/7days-golang/day7-panic-recover/gee/logger.go:15
+        /Users/7days-golang/day7-panic-recover/gee/context.go:41
+        /Users/7days-golang/day7-panic-recover/gee/router.go:99
+        /Users/7days-golang/day7-panic-recover/gee/gee.go:130
+        /usr/local/Cellar/go/1.12.5/libexec/src/net/http/server.go:2775
+        /usr/local/Cellar/go/1.12.5/libexec/src/net/http/server.go:1879
+        /usr/local/Cellar/go/1.12.5/libexec/src/runtime/asm_amd64.s:1338
 
-/*
-(3) serve static files
-$ curl http://localhost:9999/assets/css/geektutu.css
-p {
-    color: orange;
-    font-weight: 700;
-    font-size: 20px;
-}
+2020/01/09 01:00:32 [500] /panic in 395.846µs
+2020/01/09 01:00:38 [200] / in 6.985µs
 */
 import (
 	"fmt"
@@ -52,7 +51,7 @@ func FormatAsDate(t time.Time) string {
 }
 
 func main() {
-	r := gee.New()
+	r := gee.Default()
 	r.Use(gee.Logger()) // global midlleware
 	r.SetFuncMap(template.FuncMap{
 		"FormatAsDate": FormatAsDate,
@@ -76,6 +75,11 @@ func main() {
 			"title": "gee",
 			"now":   time.Date(2023, 8, 17, 0, 0, 0, 0, time.UTC),
 		})
+	})
+
+	r.GET("/panic", func(c *gee.Context) {
+		names := []string{"geektutu"}
+		c.String(http.StatusOK, names[100])
 	})
 
 	r.Run(":9999")
